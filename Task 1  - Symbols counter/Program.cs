9c82dac45f524f0b41f56cd5995ad256469eaa83
Askhat Abelseitov <askhat_abelseitov@epam.com>
2024-02-12 11:37:56 +0600

    Add project files.

---
+++ Task 1  - Symbols counter/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq; //Подключение пространства имен Linq -> методы Distinct и Count
+
+
+namespace Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter your phrase"); //Предложение ввести фразу
+
+                string phrase = Console.ReadLine(); //Метод ReadLine -> ввод переменной в консоли (строки)
+                
+                var character = phrase.Distinct(); 
+                //Cпомощью var задается локальная переменная без определенного типа -> т.к. во фразе могут быть и символы и буквы и цифры
+                //Метод Distinct удаляет дублирующиеся элементы из коллекции
+
+                Console.WriteLine("Different characters \n{0}", character.Count());
+                //Метод Count подсчитывает количество элементов коллекции, которые удовлетворяют определенному условию
+                // \n -> Перевод на другюу строку
+            }
+        }
+    }
+}
